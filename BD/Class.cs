49aed3cde9c457de72af5c3a0d63e481df8dd1e0
влад <vvladik9798@gmail.com>
2021-02-13 13:01:39 +0300

    1

---
+++ BD/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BD
+{
+    public class EMail
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BD/DBConnection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace BD
+{
+    public class DBConnection
+    {
+
+
+        private static MySqlConnection GetConnectString()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "sys";
+            string username = "root";
+            string password = "8963";
+
+            return new MySqlConnection ("Server=" + host + ";Database=" + database 
+                + ";port=" + port + ";User=" + username + ";password=" + password); 
+        }
+
+
+        public static string GetSomeData()
+        {
+            //string answer = "1 Hello i'm from Russia Vlad Prost";
+            string answer = "";
+            MySqlConnection conn = GetConnectString();
+
+
+            MySqlCommand cmd = new MySqlCommand("select * from email", conn);
+
+            conn.Open();
+
+
+
+            var reader = cmd.ExecuteReader();
+
+
+            if (reader.HasRows)
+            {
+
+                while (reader.Read())
+                {
+                    answer = answer + "1";
+                }
+            }
+
+
+            return answer;
+        }
+
+        public static bool GetBool()
+        {
+            return true;
+        }
+
+
+    }
+}
