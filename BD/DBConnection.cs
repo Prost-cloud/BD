079d48b2dc3e3971b741e12eb73854c82e110de0
влад <vvladik9798@gmail.com>
2021-02-13 18:43:48 +0300

    New connect to db added function

--- BD/DBConnection.cs
+++ BD/DBConnection.cs
@@ -4,60 +4,128 @@ using System.Linq;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data.Common;
+using System.Data;
 
 namespace BD
 {
     public class DBConnection
     {
 
-
+        private static MySqlConnection DBConnect = GetConnectString();
         private static MySqlConnection GetConnectString()
         {
             string host = "localhost";
             int port = 3306;
-            string database = "sys";
+            string database = "emails";
             string username = "root";
-            string password = "8963";
+            string password = "root";
 
-            return new MySqlConnection ("Server=" + host + ";Database=" + database 
-                + ";port=" + port + ";User=" + username + ";password=" + password); 
+            return new MySqlConnection("server=" + host + ";port=" + port +
+               ";username=" + username + ";password=" + password + ";Database=" + database);
         }
 
-
-        public static string GetSomeData()
+        private static void openConnection()
         {
-            //string answer = "1 Hello i'm from Russia Vlad Prost";
-            string answer = "";
-            MySqlConnection conn = GetConnectString();
-
-
-            MySqlCommand cmd = new MySqlCommand("select * from email", conn);
-
-            conn.Open();
-
-
-
-            var reader = cmd.ExecuteReader();
-
-
-            if (reader.HasRows)
+            if(DBConnect.State == ConnectionState.Closed)
             {
-
-                while (reader.Read())
-                {
-                    answer = answer + "1";
-                }
+                DBConnect.Open();
             }
-
-
-            return answer;
         }
-
-        public static bool GetBool()
+        
+        private static void closeConnection()
         {
-            return true;
+            if (DBConnect.State == ConnectionState.Open)
+            {
+                DBConnect.Close();
+            }
         }
 
+       
 
+        public static EMail[] SelectFromDb(int idMessage)
+        {
+
+
+            openConnection();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            string cmdText = idMessage == -1 ? "SELECT * FROM `mail` " : "SELECT * FROM `mail` WHERE `id` = @id";
+            MySqlCommand cmd = new MySqlCommand(cmdText, DBConnect);
+
+            cmd.Parameters.Add("id", MySqlDbType.Int64).Value = idMessage;
+
+            adapter.SelectCommand = cmd;
+
+            adapter.Fill(table);
+
+            closeConnection();
+
+            if (table.Rows.Count > 0) {
+                EMail[] emails = new EMail[table.Rows.Count];
+
+                for (int i = 0; i < table.Rows.Count; i++)
+                    emails[i] = new EMail(table.Rows[i]);
+
+                return emails;
+            }
+            return new EMail[0];
+        }
+
+        public static void InsertToDb(EMail mail)
+        {
+            int CurrentId = GetMaxMailId() + 1;
+
+            openConnection();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            string CmdText = "INSERT INTO `mail`(`id`, `title`, `mailfrom`, `mailto`, `message`, `date`, `authorname`) " +
+                            "VALUES (@id, @title, @mailfrom, @mailto, @message, @date, @authorname)";
+            MySqlCommand cmd = new MySqlCommand(CmdText, DBConnect);
+
+            cmd.Parameters.Add("id", MySqlDbType.Int64).Value = CurrentId;
+            cmd.Parameters.Add("title", MySqlDbType.VarChar).Value = mail.Title;
+            cmd.Parameters.Add("mailfrom", MySqlDbType.VarChar).Value = mail.MailFrom;
+            cmd.Parameters.Add("mailto", MySqlDbType.VarChar).Value = mail.MailTo;
+            cmd.Parameters.Add("message", MySqlDbType.VarChar).Value = mail.Message;
+            cmd.Parameters.Add("date", MySqlDbType.DateTime).Value = mail.Date;
+            cmd.Parameters.Add("authorname", MySqlDbType.VarChar).Value = mail.AuthorName;
+
+            adapter.InsertCommand = cmd;
+
+            cmd.ExecuteNonQuery();
+
+            closeConnection();
+
+        }
+
+        private static int GetMaxMailId()
+        {
+            openConnection();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT max(id) as id FROM mail", DBConnect);
+
+            adapter.SelectCommand = cmd;
+
+            adapter.Fill(table);
+
+            closeConnection();   
+            
+            return table.Rows[0].Field<int>("id");
+        }
+
+        public static void UpdateToDb(EMail mail)
+        {
+
+        }
     }
 }
---
+++ BD/Email.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BD
+{
+
+    public class EMail
+    {
+        public int ID { get; private set; }
+        public string Title { get; private set; }
+        public string MailFrom { get; private set; }
+        public string MailTo { get; private set; }
+        public string Message { get; private set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; private set; }
+        public string AuthorName { get; private set; }
+        public byte[] attachments { get; private set; }
+        public Tags[] tags { get; private set; }
+
+
+
+        public EMail(int ID, string title, string mailFrom, string mailTo, string message, DateTime date, string authorName)
+        {
+            this.ID = ID;
+            Title = title;
+            MailFrom = mailFrom;
+            MailTo = mailTo;
+            Message = message;
+            Date = date;
+            AuthorName = authorName;
+            //this.attachments = attachments;
+            //this.tags = tags;
+        }
+
+        //public EMail(DataTable table)
+        //{
+
+        //    this.ID = table.Rows[0].Field<int>("id");
+        //    Title = table.Rows[0].Field<string>("Title");
+        //    MailFrom = table.Rows[0].Field<string>("mailfrom");
+        //    MailTo = table.Rows[0].Field<string>("mailto");
+        //    Message = table.Rows[0].Field<string>("message");
+        //    Date = table.Rows[0].Field<DateTime>("date");
+        //    AuthorName = table.Rows[0].Field<string>("authorname");
+        //}
+
+        public EMail(System.Data.DataRow email) : this
+        (
+            email.Field<int>("id"),
+            email.Field<string>("Title"),
+            email.Field<string>("mailfrom"),
+            email.Field<string>("mailto"),
+            email.Field<string>("message"),
+            email.Field<DateTime>("date"),
+            email.Field<string>("authorname")
+        ){ }
+        
+
+        public EMail() : this(0, "", "", "", "", DateTime.Now, "") { }
+
+    }
+
+    public class Tags
+    {
+        private string Name { get; set; }
+    }
+
+
+
+}
